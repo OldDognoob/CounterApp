e60ea700a16c34005b9a61b0dee50707815e6c44
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-11-08 22:08:19 +0200

    changes

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{createContext, useContext} from 'react'
 import Number from "./Number";
 import Minus from "./Minus";
+import Add from "./Add";
+import "./style.css";
+
+
+const Num= createContext();
+
 
 export default function App(){
   return(
     <div className="App">
+      <Num.Provider value="1">
       <Number/>
       <Minus/>
+      <Add/>
+      </Num.Provider>
     </div>
   )
 };
+export {Num};
--- counter-app/src/Number.js
+++ counter-app/src/Number.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useContext}from "react";
+import {Num} from "./App.js";
 
 function Number(){
-    return <h1>1</h1>
+    const number = useContext(Num)
+return <h1>{number}</h1>
 }
 export default Number;
